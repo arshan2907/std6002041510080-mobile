2c3d45a0500d241adc03298483a4abe686592f55
อาสาฬ เครื่องสกุล <s6002041510080@Macmini213-21.local>
2019-03-28 14:29:14 +0700

    Hearder.js components

---
+++ components/Header.js
@@ -0,0 +1,18 @@
+ // import library
+ import React , {Component} from 'react';
+ import {View , Text} from 'react-native';
+ 
+
+ // create component
+class Header extends Component {
+    render() {
+        return (
+            <View>
+                <Text>{this.props.title}</Text>
+            </View>
+        );
+    }
+} 
+
+// export
+export default Header;
